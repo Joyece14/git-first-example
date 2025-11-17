6a7a85b2e45dbbadb35c76b9a9ce62016ba44114
Nikhil <2024ucp1273@mnit.ac.in>
2025-11-17 15:36:02 +0530

    commit on friends repository

--- vecadd.py
+++ vecadd.py
@@ -2,7 +2,7 @@ def vec_add(a, b):
     if len(a) != len(b):
         raise ValueError("Vectors must be the same length")
     return [a[i] + b[i] for i in range(len(a))]
-
+print("change in friends code")
 # Example
 v1 = [1, 2, 3]
 v2 = [4, 5, 6]
